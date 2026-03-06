5d7c0511d838370deaa0d93827c772fd12344c77
KENVINN <53436141+KENVINN@users.noreply.github.com>
2026-03-06 13:31:24 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import BeforeAfterSection from "@/components/BeforeAfterSection";
 import HowItWorksSection from "@/components/HowItWorksSection";
 import Footer from "@/components/Footer";
-import WhatsAppFab from "https://api.whatsapp.com/send/?phone=556592824709&text=Ol%C3%A1%2C+gostaria+de+fazer+um+or%C3%A7amento&type=phone_number&app_absent=0";
+import WhatsAppFab from "@/components/WhatsAppFab";
 
 const Index = () => {
   return (
